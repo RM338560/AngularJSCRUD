7926aea97a1aac46480a52351d6026c5548b22e6
1322115 <1322115@DESKTOP-D27445R>
2021-03-25 21:02:33 -0300

    EMPLEMENTANDO ANGULAR

---
+++ AngularJSCRUD_1/Controllers/EmployeeController.cs
@@ -0,0 +1,115 @@
+﻿using AngularJSCRUD_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularJSCRUD_1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>  
+        ///   
+        /// Get All Employee  
+        /// </summary>  
+        /// <returns></returns>  
+        public JsonResult Get_AllEmployee()
+        {
+            using (EmployeeEntities Obj = new EmployeeEntities())
+            {
+                List<Employee> Emp = Obj.Employee.ToList();
+                return Json(Emp, JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Get Employee With Id  
+        /// </summary>  
+        /// <param name="Id"></param>  
+        /// <returns></returns>  
+        public JsonResult Get_EmployeeById(string Id)
+        {
+            using (EmployeeEntities Obj = new EmployeeEntities())
+            {
+                int EmpId = int.Parse(Id);
+                return Json(Obj.Employee.Find(EmpId), JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Insert New Employee  
+        /// </summary>  
+        /// <param name="Employe"></param>  
+        /// <returns></returns>  
+        public string Insert_Employee(Employee Employe)
+        {
+            if (Employe != null)
+            {
+                using (EmployeeEntities Obj = new EmployeeEntities())
+                {
+                    Obj.Employee.Add(Employe);
+                    Obj.SaveChanges();
+                    return "Employee Added Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Inserted! Try Again";
+            }
+        }
+        /// <summary>  
+        /// Delete Employee Information  
+        /// </summary>  
+        /// <param name="Emp"></param>  
+        /// <returns></returns>  
+        public string Delete_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (EmployeeEntities Obj = new EmployeeEntities())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    if (Emp_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.Employee.Attach(Emp);
+                        Obj.Employee.Remove(Emp);
+                    }
+                    Obj.SaveChanges();
+                    return "Employee Deleted Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Deleted! Try Again";
+            }
+        }
+        /// <summary>  
+        /// Update Employee Information  
+        /// </summary>  
+        /// <param name="Emp"></param>  
+        /// <returns></returns>  
+        public string Update_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (EmployeeEntities Obj = new EmployeeEntities())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    Employee EmpObj = Obj.Employee.Where(x => x.Emp_Id == Emp.Emp_Id).FirstOrDefault();
+                    EmpObj.Emp_Age = Emp.Emp_Age;
+                    EmpObj.Emp_City = Emp.Emp_City;
+                    EmpObj.Emp_Name = Emp.Emp_Name;
+                    Obj.SaveChanges();
+                    return "Employee Updated Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Updated! Try Again";
+            }
+        }
+    }
+}
